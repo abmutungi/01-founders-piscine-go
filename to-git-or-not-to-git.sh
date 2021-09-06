7e0e126a519864a11483e927123eba2cba3dae16
Arnold Mutungi <abmutungi@gmail.com>
2021-09-06 13:57:21 +0100

    my second commit

--- to-git-or-not-to-git.sh
+++ to-git-or-not-to-git.sh
@@ -1,1 +1,1 @@
-curl -s https://learn.01founders.co/api/graphql-engine/v1/graphql --data '{"query":"{user(where:{login:{_eq:\"abmutungi\"}}){id}}"}'
+curl -s https://learn.01founders.co/api/graphql-engine/v1/graphql --data '{"query":"{user(where:{login:{_eq:\"abmutungi\"}}){id}}"}'| cut -d ":" -f4 |cut -d "}" -f1
