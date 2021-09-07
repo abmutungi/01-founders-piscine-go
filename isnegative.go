da798b4445d4f8555ed93ba9bdae85e3bec50e9a
abmutungi <abmutungi@gmail.com>
2021-09-07 17:36:51 +0100

    is Negative

--- isnegative.go
+++ isnegative.go
@@ -1,11 +1,14 @@
-package main
+package piscine
 
 import "github.com/01-edu/z01"
 
 func IsNegative(nb int) {
+	var TRune string = "T"
+	var FRune string = "F"
 	if nb >= 0 {
-		z01.Println("F")
+		z01.PrintRune(rune(FRune[0]))
 	} else {
-		z01.Println("T")
+		z01.PrintRune(rune(TRune[0]))
 	}
+	z01.PrintRune('\n')
 }
