27fa2c1b3ec9d55be38c6642c332fc3d3ed8138f
abmutungi <abmutungi@gmail.com>
2021-09-09 15:05:42 +0100

    word

--- test/main.go
+++ test/main.go
@@ -1,35 +1,13 @@
 package main
 
-import (
-	"github.com/01-edu/z01"
-)
+import piscine
 
-func PrintComb2() {
-	for a := '0'; a <= '9'; a++ {
-		for b := '0'; b <= '9'; b++ {
-			for c := '0'; c <= '9'; c++ {
-				for d := '0'; d <= '9'; d++ {
-					if c > a || c == a && d > b {
+func main () {
 
-						z01.PrintRune(a)
-						z01.PrintRune(b)
-						z01.PrintRune(32)
-						z01.PrintRune(c)
-						z01.PrintRune(d)
+		piscine.PrintStr("Hello World!")
 
-						if a == '9' && b == '8' && c == '9' && d == '9' {
-							z01.PrintRune(10)
-						} else {
-							z01.PrintRune(44)
-							z01.PrintRune(32)
-						}
-					}
-				}
-			}
-		}
-	}
 }
 
 func main() {
-	PrintComb2()
+	PrintStr()
 }
