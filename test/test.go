a17a1ab514f64180288290982adf7db7d54846ad
abmutungi <abmutungi@gmail.com>
2021-09-13 12:48:24 +0100

    recursive

--- test/test.go
+++ test/test.go
@@ -7,5 +7,18 @@ import (
 
 func main() {
 	arg := 4
+	fmt.Println(piscine.RecursiveFactorial(arg))
+}
+
+/*package main
+
+import (
+	"fmt"
+	"piscine"
+)
+
+func main() {
+	arg := 23
 	fmt.Println(piscine.IterativeFactorial(arg))
 }
+*/
