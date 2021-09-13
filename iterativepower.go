9dbae86be916a7f39a0d8837befeb7f91f140f0f
abmutungi <abmutungi@gmail.com>
2021-09-13 15:34:44 +0100

    recursive

--- iterativepower.go
+++ iterativepower.go
@@ -4,20 +4,10 @@ func IterativePower(nb int, power int) int {
 	result := 1
 	if power < 0 {
 		return 0
-	}
-
-	if power == 1 {
-		return nb
-	}
-
-	if nb == 0 {
-		return 0
-	}
-
-	if power > 1 && nb >= 1 {
+	} else {
 		for i := 1; i <= power; i++ {
 			result = result * nb
 		}
+		return result
 	}
-	return result
 }
