4d19f19ebb03afe7b67131a89a1cdbb71c7816e3
abmutungi <abmutungi@gmail.com>
2021-09-14 15:56:08 +0100

    last rune

--- alphacount.go
+++ alphacount.go
@@ -4,10 +4,13 @@ func AlphaCount(s string) int {
 	count := 0
 	result := []rune(s)
 
-for i:= 0 ; i <= len(result) ; count++{
-	if (result[i] >= 'a' && result[i] <= 'z') || (result[i] >= 'A' && result [i] <= 'Z')
-	} 
+	for i := 0; i <= len(result); i++ {
+		if (result[i] >= 'a' && result[i] <= 'z') || (result[i] >= 'A' && result[i] <= 'Z') {
+			count++
+		}
+	}
 	return count
 }
-//capital letters 65 - 90
-//lowercase 97-122
+
+// capital letters 65 - 90
+// lowercase 97-122
