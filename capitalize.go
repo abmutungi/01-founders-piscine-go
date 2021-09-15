def7f49a30d76cc195155e265b01f2cd940c867e
abmutungi <abmutungi@gmail.com>
2021-09-15 15:53:43 +0100

    capitalize

--- capitalize.go
+++ capitalize.go
@@ -1,28 +1,23 @@
 package piscine
 
 func Capitalize(s string) string {
-	a := []rune(s) 
-	var b string //create extra string
+	a := []rune(s)
 
-	for i := 0; i <= len(s)-1; i++ {
-		if a[i] >= 65 && a[i] <= 90 {
-			a[i] = a[i] + 32
+	for i := 0; i < len(s); i++ { // can also be i <= len(s)-1
+		if a[i] >= 65 && a[i] <= 90 { // if a[i] capitalised then...
+			a[i] = a[i] + 32 //...make lower case
 		}
-		b += string(a[i])
 	}
-	char := []rune(b)
-	c := ""
-	for j := 0; j <= len(b)-1; j++ {
+	char := []rune(string(a))
+	for j := 0; j < len(s); j++ {
 		if j == 0 {
-			if char[j] >= 97 && char[j] <= 122 {
+			if char[j] >= 97 && char[j] <= 122 { //
 				char[j] = char[j] - 32
 			}
 		} else if char[j-1] >= 97 && char[j-1] <= 122 || char[j-1] >= 65 && char[j-1] <= 90 || char[j-1] >= 48 && char[j-1] <= 57 {
 		} else if char[j] >= 97 && char[j] <= 122 || char[j] >= 65 && char[j] <= 90 {
 			char[j] = char[j] - 32
 		}
-
-		c += string(char[j])
 	}
-	return c
+	return string(char)
 }
--- test/test.go
+++ test/test.go
@@ -8,3 +8,15 @@ import (
 func main() {
 	fmt.Println(piscine.Capitalize("Hello! How are you? How+are+things+4you?"))
 }
+/* is upper
+package main
+
+import (
+	"fmt"
+	"piscine"
+)
+
+func main() {
+	fmt.Println(piscine.ToUpper("Hello! How are you?"))
+}
+*/
