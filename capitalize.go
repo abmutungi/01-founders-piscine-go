164341efb84d8da5bb0ea50860de35f8ab3ab76d
abmutungi <abmutungi@gmail.com>
2021-09-15 15:59:15 +0100

    capitalize

--- capitalize.go
+++ capitalize.go
@@ -1,16 +1,16 @@
 package piscine
 
 func Capitalize(s string) string {
-	a := []rune(s)
+	a := []rune(s) // slicing the string s
 
 	for i := 0; i < len(s); i++ { // can also be i <= len(s)-1
 		if a[i] >= 65 && a[i] <= 90 { // if a[i] capitalised then...
 			a[i] = a[i] + 32 //...make lower case
 		}
 	}
-	char := []rune(string(a))
+	char := []rune(string(a)) //slicing the string created above
 	for j := 0; j < len(s); j++ {
-		if j == 0 {
+		if j == 0 { // capitalising the first character if its a lowercase letter
 			if char[j] >= 97 && char[j] <= 122 { //
 				char[j] = char[j] - 32
 			}
