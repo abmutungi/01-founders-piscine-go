f68f207c325785632e776e026d5ae607cd853a65
abmutungi <abmutungi@gmail.com>
2021-09-15 12:06:38 +0100

    isalpha

--- isalpha.go
+++ isalpha.go
@@ -4,7 +4,7 @@ func IsAlpha(s string) bool {
 	rstr := []rune(s)
 
 	for _, i := range rstr {
-		if i >= 0 && i <= 32 {
+		if i >= 0 && i <= 47 || i >= 58 && i <= 64 || i >= 91 && i <= 96 || i >= 123 {
 			return false
 		}
 	}
