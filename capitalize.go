8a6b7f1956930c375211eb9165383ea76cf03312
abmutungi <abmutungi@gmail.com>
2021-09-15 16:14:03 +0100

    capitalize

--- capitalize.go
+++ capitalize.go
@@ -4,14 +4,14 @@ func Capitalize(s string) string {
 	a := []rune(s) // slicing the string s
 
 	for i := 0; i < len(s); i++ { // can also be i <= len(s)-1
-		if a[i] >= 65 && a[i] <= 90 { // if a[i] capitalised then...
+		if a[i] >= 65 && a[i] <= 90 { // if a capitalised then...
 			a[i] = a[i] + 32 //...make lower case
 		}
 	}
-	char := []rune(string(a)) //slicing the string created above
+	char := []rune(string(a)) // slicing the string created above
 	for j := 0; j < len(s); j++ {
 		if j == 0 { // capitalising the first character if its a lowercase letter
-			if char[j] >= 97 && char[j] <= 122 { //
+			if char[j] >= 97 && char[j] <= 122 {
 				char[j] = char[j] - 32
 			}
 		} else if char[j-1] >= 97 && char[j-1] <= 122 || char[j-1] >= 65 && char[j-1] <= 90 || char[j-1] >= 48 && char[j-1] <= 57 {
--- test/test.go
+++ test/test.go
@@ -8,6 +8,7 @@ import (
 func main() {
 	fmt.Println(piscine.Capitalize("Hello! How are you? How+are+things+4you?"))
 }
+
 /* is upper
 package main
 
