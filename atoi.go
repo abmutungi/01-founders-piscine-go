f78ceedd4d88a137b302f5ff714542e55d2d98c4
abmutungi <abmutungi@gmail.com>
2021-09-17 08:32:01 +0100

    atoi

--- atoi.go
+++ atoi.go
@@ -1,12 +1,27 @@
 package piscine
 
-func Atoi(s string) int{
-
-:= rune(s)
-trim := 0
-
-for i := 0 ; i < len(s) ; i++{
-
-
-}
+func Atoi(s string) int {
+	str := []rune(s)
+	neg := str[0] == '-'
+	if neg || str[0] == '+' {
+		str = str[1:]
+	}
+	trim := 0
+	for {
+		if str[0] >= '0' && str[0] <= '9' {
+			trim *= 10
+			trim += int(str[0] - 48)
+		} else {
+			return 0
+		}
+		if len(str) > 1 {
+			str = str[1:]
+		} else {
+			break
+		}
+	}
+	if neg {
+		trim *= -1
+	}
+	return trim
 }
