61cb8de443a2a76f337404e5b63039806488e816
abmutungi <abmutungi@gmail.com>
2021-09-20 13:20:16 +0100

    split

--- concatparams.go
+++ concatparams.go
@@ -1,13 +1,9 @@
 package piscine
 
 func ConcatParams(args []string) string {
-	str := ""
-
-	for i, result := range args {
-		str += string(result)
-		if i != len(args)-1 {
-			str += "\n"
-		}
-	}
-	return str
+	var argument string = args[0] // 0 to print the 1st string
+	for _, s := range args[1:] {  // range loop args[1:] is set to print the following arguments
+		argument += "\n" + s // (+=) is add and assign- add var (a) with new lines and the other arguments
+	} // "\n" has to go first before adding (s) for it to print each argument on a separate line
+	return argument
 }
--- splitwhitespaces.go
+++ splitwhitespaces.go
@@ -1,8 +1,24 @@
 package piscine
 
-import "strings"
-
 func SplitWhiteSpaces(str string) []string {
-	split := strings.Fields(str)
-	return split
+	runes := []rune(str) // break string into runes
+
+	var answer []string // creat empty string array
+
+	result := "" // intialise result with empty string
+
+	for i := 0; i < len(runes); i++ {
+		if runes[i] == ' ' || runes[i] == '\n' || runes[i] == '\t' {
+			if result != "" { // result updates every interation
+				answer = append(answer, result) // append previous result
+				result = ""                     // update result to be ""
+			}
+		} else {
+			result = result + string(runes[i])
+		}
+		if i == len(runes)-1 {
+			answer = append(answer, result) // append last result
+		}
+	}
+	return answer
 }
