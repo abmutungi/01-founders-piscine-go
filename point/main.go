d1c11eb441ebda5b27eb6906f917536fd5de0e6c
abmutungi <abmutungi@gmail.com>
2021-09-20 17:59:35 +0100

    point

--- point/main.go
+++ point/main.go
@@ -29,6 +29,9 @@ func main() {
 	z01.PrintRune(' ')
 	z01.PrintRune('y')
 	z01.PrintRune(' ')
+	z01.PrintRune('=')
+	z01.PrintRune(' ')
 	z01.PrintRune('2')
 	z01.PrintRune('1')
+	z01.PrintRune('\n')
 }
