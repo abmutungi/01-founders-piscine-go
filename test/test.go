93a8a29c78c580018b52d45ccf05e5016e77b5bc
abmutungi <abmutungi@gmail.com>
2021-09-22 18:41:08 +0100

    rot14

--- test/test.go
+++ test/test.go
@@ -1,15 +1,16 @@
 package main
 
-import ( "piscine"
-			"github.com/01-edu/z01"
+import (
+	"piscine"
+
+	"github.com/01-edu/z01"
 )
 
-func main(){
+func main() {
+	result := piscine.Rot14("Hello! How are You?")
 
-		result := piscine.Rot14("Hello! How are You?")
-
-		for _, r := range result {
-			z01.PrintRune(r)
-		}
-			z01.PrintRune('\n')
+	for _, r := range result {
+		z01.PrintRune(r)
+	}
+	z01.PrintRune('\n')
 }
