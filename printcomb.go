583e8d64b5d9333f1e69bb5907018d0d820175a9
abmutungi <abmutungi@gmail.com>
2021-09-22 18:39:16 +0100

    rot14

--- printcomb.go
+++ printcomb.go
@@ -1,26 +1,24 @@
 package piscine
 
-func PrintComb(){
+import "github.com/01-edu/z01"
 
+func PrintComb() {
+	for a := '0'; a <= '9'; a++ {
+		for b := '0'; a <= '9'; b++ {
+			for c := '0'; a <= '9'; c++ {
+				if a < b && b < c {
 
-	for a := 0, ; a <= 9 ; a++{
-		for b := 0, ; a <= 9 ; b++{
-			for c := 0, ; a <= 9 ; c++{
+					z01.PrintRune(a)
+					z01.PrintRune(b)
+					z01.PrintRune(c)
 
-						if a < b && b < c {
-
-								z01.PrintRune(a)
-								z01.PrintRune(b)
-								z01.PrintRune(c)
-
-					if a == 7 && b == 8 && c == 9 {
-								z01.PrintRune('\n')
-								} else {
-									z01.PrintRune(',')
-									z01.PrintRune(' ')
-							}
-						}
+					if a == '7' && b == '8' && c == '9' {
+						z01.PrintRune('\n')
+					} else {
+						z01.PrintRune(',')
+						z01.PrintRune(' ')
 					}
+				}
 			}
 		}
 	}
