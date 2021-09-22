d013b792fcb8f2081837cc8ec52488089f8a0657
abmutungi <abmutungi@gmail.com>
2021-09-22 12:45:42 +0100

    issorted

--- issorted.go
+++ issorted.go
@@ -1,18 +1,17 @@
 package piscine
 
 func IsSorted(f func(a, b int) int, a []int) bool {
-
-	if len(tab) > 1{
-		if f(tab[0],tab[1]) >= 0 {
-			for i := 0; i < len(tab) - 1 ; i++{
-				if f(tab[i],tab[i+1]) < 0 {
+	if len(a) > 1 {
+		if f(a[0], a[1]) >= 0 {
+			for i := 0; i < len(a)-1; i++ {
+				if f(a[i], a[i+1]) < 0 {
 					return false
 				}
 			}
 		}
-		if f(tab[0], tab[1]) <= 0 {
-			for i := 0 ; i < len(tab) - 1 : i++{
-				if f(tab[i]), tab[i+1]) > 0 {
+		if f(a[0], a[1]) <= 0 {
+			for i := 0; i < len(a)-1; i++ {
+				if f(a[i], a[i+1]) > 0 {
 					return false
 				}
 			}
